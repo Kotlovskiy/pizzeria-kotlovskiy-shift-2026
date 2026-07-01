284207ff6299820fa96e5d0ae64c54a3f2ae7120
Yaroslav Kotlovskiy <ya111ep@mail.ru>
2026-07-01 16:28:27 +0700

    Упростил классы хранения данных: в AppDataStore избавился от кеширования ключей и убрал явное переключение пула потоков. Из EncryptionAppDataStore убрал дублирование логики, сделав его обёрткой над AppDataStore

--- core/storage/src/main/java/com/example/storage/EncryptionAppDataStore.kt
+++ core/storage/src/main/java/com/example/storage/EncryptionAppDataStore.kt
@@ -1,60 +1,29 @@
 package com.example.storage
 
-import android.content.Context
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory.create
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStoreFile
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
-import java.util.concurrent.ConcurrentHashMap
 
 class EncryptionAppDataStore(
-    context: Context,
+    private val appDataStore: AppDataStore,
     private val encryptionManager: EncryptionManager
 ) {
-    private val dataStore = create {
-        context.preferencesDataStoreFile(DATA_STORE_NAME)
-    }
-
-    suspend fun getString(key: String): String {
-        return withContext(Dispatchers.IO) {
-            encryptionManager.decrypt(dataStore.data.first()[getPrefKey(key)] ?: "")
+    suspend fun getString(key: String): String? {
+        return appDataStore.getString(key)?.let { encrypted ->
+            encryptionManager.decrypt(encrypted)
         }
     }
 
     suspend fun setString(key: String, value: String) {
-        return withContext(Dispatchers.IO) {
-            dataStore.edit {
-                it[getPrefKey(key)] = encryptionManager.encrypt(value)
-            }
-        }
+        appDataStore.setString(key, encryptionManager.encrypt(value))
     }
 
     suspend fun clearString(key: String) {
-        withContext(Dispatchers.IO) {
-            dataStore.edit {
-                it.remove(getPrefKey(key))
-            }
-        }
+        appDataStore.clearString(key)
     }
 
     fun observeString(key: String): Flow<String?> {
-        return dataStore.data.map {
-            encryptionManager.decrypt(it[getPrefKey(key)] ?: "")
+        return appDataStore.observeString(key).map { encrypted ->
+            encrypted?.let { encryptionManager.decrypt(it) }
         }
-    }
-
-    private fun getPrefKey(key: String): Preferences.Key<String> {
-        return keys.getOrPut(key) { stringPreferencesKey(key) }
-    }
-
-    companion object {
-        private const val DATA_STORE_NAME = "encrypted_datastore"
-        private val keys = ConcurrentHashMap<String, Preferences.Key<String>>()
     }
 }
