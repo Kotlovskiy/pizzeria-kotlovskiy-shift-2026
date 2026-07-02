3d9ad84bf94fc7aed0d7699107dbb0664522feb7
Yaroslav Kotlovskiy <ya111ep@mail.ru>
2026-07-02 16:32:27 +0700

    Добавил создание апи в DI модуль

--- core/network/src/main/java/com/example/network/NetworkConstants.kt
+++ core/network/src/main/java/com/example/network/NetworkConstants.kt
@@ -1,7 +1,7 @@
 package com.example.network
 
 object NetworkConstants {
-    const val BASE_URL = ""
+    const val BASE_URL = "https://juniorsbootcamp.ru/"
     const val APPLICATION_JSON = "application/json"
     const val AUTHORIZATION_HEADER = "Authorization"
     const val BEARER = "Bearer"
--- core/network/src/main/java/com/example/network/NetworkModule.kt
+++ core/network/src/main/java/com/example/network/NetworkModule.kt
@@ -2,6 +2,10 @@ package com.example.network
 
 import com.example.network.NetworkConstants.APPLICATION_JSON
 import com.example.network.NetworkConstants.BASE_URL
+import com.example.network.api.AuthApi
+import com.example.network.api.OrderApi
+import com.example.network.api.PizzaApi
+import com.example.network.api.UserApi
 import com.example.storage.EncryptedDataStore
 import com.example.storage.IDataStore
 import dagger.Module
@@ -14,6 +18,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -91,4 +96,24 @@ object NetworkModule {
             .client(okHttpClient)
             .addConverterFactory(json.asConverterFactory(APPLICATION_JSON.toMediaType()))
             .build()
+
+    @Provides
+    fun provideAuthApi(
+        @UnauthorizedRetrofitClient client: Retrofit
+    ): AuthApi = client.create<AuthApi>()
+
+    @Provides
+    fun provideOrderApi(
+        @AuthorizedRetrofitClient client: Retrofit
+    ): OrderApi = client.create<OrderApi>()
+
+    @Provides
+    fun providePizzaApi(
+        @UnauthorizedRetrofitClient client: Retrofit
+    ): PizzaApi = client.create<PizzaApi>()
+
+    @Provides
+    fun provideUserApi(
+        @AuthorizedRetrofitClient client: Retrofit
+    ): UserApi = client.create<UserApi>()
 }
